748a7c7410dc66180ffb9f93cac01cefdb1ac356
Sebastian Andres Quispe Barriga <sebastian.quispe.b@utec.edu.pe>
2020-04-19 17:39:55 -0500

    Node.h implemented

---
+++ Node.h
@@ -0,0 +1,9 @@
+template <typename T>
+class Node {
+    public:
+        T value;
+        Node<T> *next;
+        Node(){value= NULL;  next =nullptr;}
+        Node(T a, Node<T> * b): value (a), next(b){}
+        ~Node(){value= NULL; next= nullptr;}     
+};
